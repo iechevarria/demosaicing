7bf434a8b3f62712d5b0c24333f6366c9e4c54cf
Ivan Echevarria <iechevarria@email.wm.edu>
2016-10-17 00:01:51 -0400

    Added X-Trans simulation
    X-Trans and Bayer data generation now both work

--- remosaic.py
+++ remosaic.py
@@ -2,74 +2,117 @@ from PIL import Image
 import numpy as np
 
 
-def toBayer(img, x_offset = 0, y_offset = 0):
+def to_bayer(img, x_offset = 0, y_offset = 0):
 	""" Eliminates channel information to simulate a Bayer CFA 
-		@param 3d numpy array img
+		@param 3D numpy array img
 		@param int x_offset
 		@param int y_offset
 		@return img """
 
-	for y in range(int(img.shape[0])):
-		for x in range(int(img.shape[1])):
-			
+	for i in range(int(img.shape[0])):
+		y = i + y_offset
+		for j in range(int(img.shape[1])):
+			x = j + x_offset
+			dy = y % 2
+			dx = x % 2
 			# blue row elimination
-			if (y + y_offset) % 2 == 0:
-
+			if dy == 0:
 				# delete blue, red
-				if (x + x_offset) % 2 == 0:
-					img[y][x][2] = img[y][x][0] = 0
+				if dx == 0:
+					img[i][j][2] = img[i][j][0] = 0
 				# delete green, red
 				else:
-					img[y][x][1] = img[y][x][0] = 0
+					img[i][j][1] = img[i][j][0] = 0
 
 			# red row elimination
 			else:
-
 				# delete green, blue
-				if (x + x_offset) % 2 == 0:
-					img[y][x][1] = img[y][x][2] = 0
+				if dx == 0:
+					img[i][j][1] = img[i][j][2] = 0
 				# delete red, blue
 				else:
-					img[y][x][0] = img[y][x][2] = 0
+					img[i][j][0] = img[i][j][2] = 0
 
 	return img
 
 
-def toXtrans(img, x_offset = 0, y_offset = 0):
+def to_xtrans(img, x_offset = 0, y_offset = 0):
 	""" Eliminates channel information to simulate an X-Trans CFA 
-		@TODO implement channel removal"""
+		@param 3D numpy array img
+		@param int x_offset
+		@param int y_offset 
+		@return img """
 		
-	for y in range(int(img.shape[1])):
-		for x in range(int(img.shape[2])):
-
+	for i in range(int(img.shape[0])):
+		y = i + y_offset
+		for j in range(int(img.shape[1])):
+			x = j + x_offset
+			dy = y % 6
+			dx = x % 6
 			# row 0 and 2 elminiation
-			if (y + y_offset) % 6 == 0 or (y + y_offset) % 6 == 2:
-				 # delete red, green
-
-				 # delete blue, green
-
-				 # delete red, blue
+			if dy in [0,2]:
+				# blue: delete red, green
+				if dx == 1:
+					img[i][j][0] = img[i][j][1] = 0
+				# red: delete blue, green
+				elif dx == 4: 
+					img[i][j][2] = img[i][j][1] = 0
+				# green: delete red, blue
+				else:
+					img[i][j][0] = img[i][j][2] = 0
 
 			# row 1 elimination
-			elif (y + y_offset) % 6 == 1:
-				continue
+			elif dy == 1:
+				# blue: delete red, green
+				if dx in [3,5]:
+					img[i][j][0] = img[i][j][1] = 0
+				# green: delete red, blue
+				elif dx in [1,4]:
+					img[i][j][0] = img[i][j][2] = 0
+				# red: delete blue, green
+				else:
+					img[i][j][2] = img[i][j][1] = 0
+
 			# row 3 and 5 elimination
-			elif (y + y_offset) % 6 == 3 or (y + y_offset) % 6 == 5:
-				continue
-			# row 5 elimnination
+			elif dy in [3,5]:
+				# blue: delete red, green
+				if dx == 4: 
+					img[i][j][0] = img[i][j][1] = 0
+				# red: delete blue, green
+				elif dx == 1:
+					img[i][j][2] = img[i][j][1] = 0
+				# green: delete red, blue
+				else: 
+					img[i][j][0] = img[i][j][2] = 0
+
+			# row 4 elimination
 			else:
-				continue
+				# blue: delete red, green
+				if dx in [0,2]:
+					img[i][j][0] = img[i][j][1] = 0
+				# green: delete red, blue
+				elif dx in [1,4]:
+					img[i][j][0] = img[i][j][2] = 0
+				# red: delete blue, green
+				else:
+					img[i][j][2] = img[i][j][1] = 0
+				
+	return img
 
 
 def main():
 	print("Basic Pattern Test")
-	img = np.zeros((100,100,3), np.uint8)
+	img = np.zeros((10,10,3), np.uint8)
 	img[0:100,0:100,0:3] = 255
 	out1 = Image.fromarray(img, 'RGB')
-	out1.show()
-	img = toBayer(img)
-	out2 = Image.fromarray(img, 'RGB')
-	out2.show()
+	out1.save("out1.png")
+	bayer = to_bayer(img)
+	out2 = Image.fromarray(bayer, 'RGB')
+	out2.save("out2.png")
+	img[0:100,0:100,0:3] = 255
+	xtrans = to_xtrans(img)
+	out3 = Image.fromarray(xtrans, 'RGB')
+	out3.save("out3.png")
 
 
 if __name__ == '__main__':
