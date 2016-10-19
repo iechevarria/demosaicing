0a250c22a84b8cfd5ff2e873bc09101fccd0ea5a
Ivan Echevarria <iechevarria@email.wm.edu>
2016-10-19 00:45:00 -0400

    Simplified nearest_neighbor
    added assumption of even dimensions to implement nearest_neighbor for
    2x2 neighborhoods

--- nearest_neighbor.py
+++ nearest_neighbor.py
@@ -4,63 +4,23 @@ from PIL import Image
 import numpy as np
 
 def nn_demosaic_bayer(img, x_offset = 0, y_offset = 0):
-	""" Replaces channel information by taking the nearest neighbor's value """
+	""" Replaces channel information by taking the nearest neighbor's value
+		Assumption: img dimensions are divisible by 2
+		TODO add offsets back in """
 
-	for i in range(int(img.shape[0])):
-		dy = (i + y_offset) % 2
-		for j in range(int(img.shape[1])):
-			dx = (j + x_offset) % 2
+	for i in xrange(0, int(img.shape[0]), 2):
+		for j in xrange(0, int(img.shape[1]), 2):
 
-			# blue/green row
-			if dy == 0: 
-				# green: replace red and blue 
-				if dx == 0:
-					if i + 1 < int(img.shape[0]):
-						img[i][j][0] = img[i+1][j][0]
-					else:
-						img[i][j][0] = img[i-1][j][0]
-					if j + 1 < int(img.shape[1]):
-						img[i][j][2] = img[i][j+1][2]
-					else:
-						img[i][j][2] = img[i][j-1][2]
-
-				# blue: replace green and red
-				else:
-					if i + 1 < int(img.shape[0]):
-						img[i][j][1] = img[i+1][j][1]
-					else:
-						img[i][j][1] = img[i-1][j][1]
-					if i + 1 < int(img.shape[0]) and j + 1 < int(img.shape[1]):
-						img[i][j][0] = img[i+1][j+1][0]
-					else:
-						img[i][j][0] = img[i-1][j-1][0]
-
-			# red/green row
-			else:
-				# red: replace green and blue 
-				if dx == 0:
-					if i + 1 < int(img.shape[0]):
-						img[i][j][1] = img[i+1][j][1]
-					else:
-						img[i][j][1] = img[i-1][j][1]
-					if i + 1 < int(img.shape[0]) and j + 1 < int(img.shape[1]):
-						img[i][j][2] = img[i+1][j+1][2]
-					else:
-						img[i][j][2] = img[i-1][j-1][2]
-
-				# green: replace red and blue 
-				else:
-					if j + 1 < int(img.shape[0]):
-						img[i][j][0] = img[i][j+1][0]
-					else:
-						img[i][j][0] = img[i][j-1][0]
-					if i + 1 < int(img.shape[1]):
-						img[i][j][2] = img[i+1][j][2]
-					else:
-						img[i][j][2] = img[i-1][j][2]
+			# replace red values
+			img[i][j][0] = img[i][j+1][0] = img[i+1][j+1][0] = img[i+1][j][0]
+			# replace blue values
+			img[i][j][2] = img[i+1][j][2] = img[i+1][j+1][2] = img[i][j+1][2]
+			# replace green values
+			img[i][j+1][1] = img[i][j][1]
+			img[i+1][j][1] = img[i+1][j+1][1]
 
 	return img
-
+	
 
 def nn_demosaic_xtrans(img, x_offset, y_offset):
 	""" Replaces channel information by taking the nearest neighbor's value """
