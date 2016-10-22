ba7704e4c43390da4b3bad1f792bf394b332bcd8
Ivan Echevarria <iechevarria@email.wm.edu>
2016-10-21 21:57:18 -0400

    begin bilinear interpolation

---
+++ bilinear_interpolation.py
@@ -0,0 +1,13 @@
+""" Use bilinear interpolation algorithm to replace channel information """
+
+from PIL import Image
+import numpy as np
+
+def bi_bayer(img, x_offset = 0, y_offset = 0):
+    """
+    """
+    
+
+def pad_img(img, radius):
+    """pads image """
+    padded_img = img
